3f979ecbc33bf5affa59b559c5468a060769f453
jloffler <48938126+jloffler@users.noreply.github.com>
2019-04-27 17:25:36 -0700

    .

--- 8.1-retry.js
+++ 8.1-retry.js
@@ -25,5 +25,17 @@ function reliableMultiply(a, b) {
   }
 }
 
+// test
 console.log(reliableMultiply(8, 8));
 // → 64
+
+/*
+Retry
+
+Say you have a function primitiveMultiply that in 20 percent of cases multiplies
+two numbers and in the other 80 percent of cases raises an exception of type
+MultiplicatorUnitFailure. Write a function that wraps this clunky function and
+just keeps trying until a call succeeds, after which it returns the result.
+
+Make sure you handle only the exceptions you are trying to handle.
+*/
